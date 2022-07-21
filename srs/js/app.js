8012d6565514499b2e715862d285418bd6093231
Aleksandr <biriukov.a94@gmail.com>
2022-07-21 16:23:55 +0500

    fixed questions

--- srs/js/app.js
+++ srs/js/app.js
@@ -9,8 +9,31 @@ const personalMovieDB = {
    privat: false,
 }
 
-const questionLastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-      questionGredeFilm = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 1; i++){
+   const questionLastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+         questionGredeFilm = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[questionLastFilm] = questionGredeFilm;
+         if (questionLastFilm != null && questionGredeFilm != null && questionLastFilm != '' && questionGredeFilm != '' && questionLastFilm.length < 50) {
+            personalMovieDB.movies[questionLastFilm] = questionGredeFilm;
+            console.log('Результат ответов записан');
+         } else {
+            console.log('Ошибка');
+            i--;
+         }
+         
+}
+if (personalMovieDB.count < 10){
+   console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+   console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+   console.log('Вы киноман');
+} else {
+   console.log('Произошла ошибка');
+}
 console.log(personalMovieDB);
+
+
+
+
+
