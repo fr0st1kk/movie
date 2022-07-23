85c1841f0d558d12f547d3e084cbf394493bec15
Aleksandr <biriukov.a94@gmail.com>
2022-07-23 22:32:35 +0500

    reload

--- srs/js/app.js
+++ srs/js/app.js
@@ -78,9 +78,26 @@ const personalMovieDB = {
    });
 }
 }
-personalMovieDB.start();
-personalMovieDB.rememberMyFilms()
-personalMovieDB.detectPersonalLevel()
-personalMovieDB.showMyDb()
-personalMovieDB.writeYourGenres()
-personalMovieDB.toggleVisibleMyDB()
+//personalMovieDB.start();
+//personalMovieDB.rememberMyFilms()
+//personalMovieDB.detectPersonalLevel()
+//personalMovieDB.showMyDb()
+//personalMovieDB.writeYourGenres()
+//personalMovieDB.toggleVisibleMyDB()
+
+function createCounter() {
+   let counter = 0;
+
+   const myFunction= function() {
+      counter = counter + 1;
+      return counter;
+   }
+
+   return myFunction;
+}
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1, c2, c3);
