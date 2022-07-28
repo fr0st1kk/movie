6f9e4004fa68e29aea08308fd08336dcd97ee0ae
Aleksandr <biriukov.a94@gmail.com>
2022-07-28 23:31:37 +0500

    add functionals

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-/* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
@@ -23,4 +11,29 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+//1)
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+      
+adv.forEach(element => {
+   element.remove()
+});
+
+genre.textContent = 'Драма'
+
+poster.style.cssText = 'background: url(img/bg.jpg) center center/cover no-repeat;'
+
+movieList.innerHTML = '';
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+   movieList.innerHTML += `
+   <li class="promo__interactive-item">${i + 1 + '.'} ${film}
+   <div class="delete"></div>
+   </li>
+   `;
+});
 
